872bd0e26bdd9c7bfe8adda6ff8d9a569c7910e0
Hubertas Klangauskas <93277255+Harbertas@users.noreply.github.com>
2022-09-17 13:52:32 +0300

    Add files via upload

--- Input.h
+++ Input.h
@@ -8,5 +8,6 @@ class Input
 	public:
 		inline double getTimeTaken() const { return timeTaken; };
 		inline double setTimeTaken(double time) { this->timeTaken = time; };
+		std::istream read(vector<string>& row);
 };
 
