cd1cd77e37c16037c78911a87e85cb49b4a2050d
Hubertas Klangauskas <93277255+Harbertas@users.noreply.github.com>
2022-09-29 10:05:57 +0300

    Update test.cpp

--- GoogleTest/test.cpp
+++ GoogleTest/test.cpp
@@ -2,10 +2,6 @@
 #include "../hash v0.1/input.h"
 #include "../hash v0.1/input.cpp"
 
-//TEST(TestCaseName, TestName) {
-//  EXPECT_EQ(1, 1);
-//  EXPECT_TRUE(true);
-//}
 
 TEST(testHash, hashSize) {
 	Input data;
@@ -20,38 +16,3 @@ TEST(testHash, hashDifference) {
 	data2.hashRow("Hubertas");
 	ASSERT_NE(data.getHashedRow(), data2.getHashedRow());
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*TEST(TestCaseName1, checkConstructor) {
-	duomenys pirmas({0,0,0},3,4,6,"jonas","jonaitis");
-	ASSERT_EQ(pirmas.vardas(), "antanas");
-}
-TEST(TestCaseName2, CheckSetterAndAssignment) {
-	duomenys pirmas, antras;
-	pirmas.setEgz(3);
-	antras = pirmas;
-	ASSERT_EQ(pirmas.egz(), 3);
-}
-TEST(TestCaseName3, CheckMarksQuantity) {
-	duomenys pirmas;
-	pirmas.setPaz(3);
-	pirmas.setPaz(4);
-	pirmas.setPaz(5);
-	ASSERT_EQ(3, pirmas.kiekPaz());
-}*/
